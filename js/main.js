b32f7b02b471312d721df04ab001fa245444a9bd
Mohammed Abosalem <m.salem@altairgroup.eu>
2020-02-20 13:50:38 +0200

    add js code to order menu

--- js/main.js
+++ js/main.js
@@ -91,3 +91,125 @@ $(document).ready(function() {
     $(".rotate").toggleClass("down");
   });
 });
+
+// add items to cart
+const restName = document.querySelector(".order-content .r-name");
+const restAddress = document.querySelector(".order-content .r-address");
+
+const addToCartBtn = document.getElementById("addToCart");
+addToCartBtn.addEventListener("click", function(e) {
+  // get checked value from Size radio buttons
+  const radioSizeBtns = document.getElementsByName("size");
+  let checkedSizeVal;
+  radioSizeBtns.forEach(radioSize => {
+    if (radioSize.checked) {
+      checkedSizeVal = radioSize.value;
+    }
+  });
+
+  // get checked value from Milk radio buttons
+  const radioMilkBtns = document.getElementsByName("milk");
+  let checkedMilkVal;
+  radioMilkBtns.forEach(radioMilk => {
+    if (radioMilk.checked) {
+      checkedMilkVal = radioMilk.value;
+    }
+  });
+
+  // get checked value from Modification checkbox
+  const feauresBtns = document.getElementsByName("options[]");
+  let checkedFeatureVal;
+  feauresBtns.forEach(feature => {
+    if (feature.checked) {
+      checkedFeatureVal = feature.value;
+    }
+  });
+
+  // get checked value from Sugar radio buttons
+  const radioSugarBtns = document.getElementsByName("sugar");
+  let checkedSugarVal;
+  radioSugarBtns.forEach(radioSugar => {
+    if (radioSugar.checked) {
+      checkedSugarVal = radioSugar.value;
+    }
+  });
+
+  console.log(`${restName.textContent} 
+               ${restAddress.textContent} `);
+  console.log(`Size: ${checkedSizeVal}`);
+  console.log(`Milk: ${checkedMilkVal}`);
+  console.log(`Sugar: ${checkedSugarVal} sugar`);
+  console.log(`${checkedFeatureVal}`);
+  e.preventDefault();
+});
+
+// create tags input
+const tagContainer = document.querySelector(".tag-container");
+const input = document.querySelector(".tag-container input");
+
+let tags = [];
+
+function createTag(label) {
+  const div = document.createElement("div");
+  const span = document.createElement("span");
+  const iClose = document.createElement("i");
+  iClose.setAttribute("id", "close");
+  iClose.setAttribute("data-item", label);
+
+  div.setAttribute("class", "tag");
+  span.innerText = label;
+  iClose.setAttribute("class", "fas fa-times");
+
+  div.appendChild(span);
+  div.appendChild(iClose);
+
+  return div;
+}
+
+// reset function
+function reset() {
+  document.querySelectorAll(".tag").forEach(function(tag) {
+    tag.parentElement.removeChild(tag);
+  });
+}
+
+function removeTag() {
+  // const items = document.querySelectorAll(".tag span");
+  // items.forEach(function(item) {
+  //   console.log(item.textContent);
+  //   // const index = tags.indexOf();
+  //   // let newArr = tags.splice(index, 1);
+  //   // console.log(index);
+}
+
+// add values to tags array
+function addTags() {
+  reset();
+  tags
+    .slice()
+    .reverse()
+    .forEach(function(tag) {
+      const input = createTag(tag);
+      tagContainer.prepend(input);
+    });
+  console.log(tags);
+}
+
+input.addEventListener("keyup", function(e) {
+  if (e.key === "Enter") {
+    tags.push(input.value); // add values to array
+    addTags(); // show values on input
+    input.value = ""; // reset input
+  }
+});
+
+// delete item when click on x close
+document.addEventListener("click", function(e) {
+  if (e.target.nodeName === "I") {
+    const value = e.target.getAttribute("data-item");
+    const index = tags.indexOf(value);
+    tags = [...tags.slice(0, index), ...tags.slice(index + 1)];
+    console.log(tags);
+    addTags();
+  }
+});
